dafcc1adb52b7ba6bb56f14277e2201effe5ee3c
Michael Capelli <michael.capelli@ufl.edu>
2026-04-20 23:09:31 -0400

    had some cin parsing issues

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@ int main() {
         return -1;
     }
 
+    // gets rid of the \n stuff of the first line
+    std::string dummy;
+    std::getline(std::cin, dummy);
+
     for (int i = 0; i < n; i++) {
         std::string line;
 
@@ -31,10 +35,14 @@ int main() {
             while (current.front() == '\"' && current.back() != '\"') {
                 std::string nextName;
                 lineStream >> nextName;
-                current += nextName;
+                current += " " + nextName;
+            }
+            if (current.front() == '\"' && current.back() == '\"' && current.length() >= 2) {
+                current = current.substr(1, current.length() - 2);
             }
             arguments.push_back(current);
         }
+        
         int result = compass.parseCommand(command, arguments);
         if (result == 1)
             std::cout << "successful" << std::endl;
